af818fadc103f080c0ff7130beba5fdb890c42b1
mdshadab41 <shadabjhu00447@gmail.com>
2023-11-06 15:14:19 +0530

    commit-12

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "create-a-rgb-color-app"
+include(":app")
+ 
